a4262d64c6be02857a8370c48ff97c6be10bbe0f
risaito74 <to_saitoh@hotmail.com>
2018-05-11 12:55:43 +0900

    1st Commit

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+    GameObject hpGage;
+
+	// Use this for initialization
+	void Start () {
+        this.hpGage = GameObject.Find("hpGage");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DecreaseHp()
+    {
+        this.hpGage.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+
+}
